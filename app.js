58144cdddef146bec7fca95c52bcbcfc24fb0b3d
SaraEOlivera <saraeolivera@gmail.com>
2025-11-03 09:29:32 -0300

    Ejercicio 2

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 /* 1. Calcular el área de un rectángulo */
 
-function calcularAreaRectangulo(ancho, alto) {
+const calcularAreaRectangulo = (ancho, alto) =>  {
     let area = ancho * alto;
-    return area
-}
+    return area;
+} 
 
 console.log(calcularAreaRectangulo(5,3));
 
 
+
 /* 2. Contar palabras en una cadena */
+
+const contarPalabras = texto => {
+    let palabras = texto.trim().split(" ");
+    console.log(palabras);
+    console.log(palabras.length);
+}
+
+console.log(contarPalabras("Humahuaca es un lugar copado"));
