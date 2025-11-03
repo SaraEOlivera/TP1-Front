4588365f73af5aaeda1afcf6725b61836571b75b
SaraEOlivera <saraeolivera@gmail.com>
2025-11-03 08:39:11 -0300

    Ejercicio 1

---
+++ app.js
@@ -0,0 +1,11 @@
+/* 1. Calcular el área de un rectángulo */
+
+function calcularAreaRectangulo(ancho, alto) {
+    let area = ancho * alto;
+    return area
+}
+
+console.log(calcularAreaRectangulo(5,3));
+
+
+/* 2. Contar palabras en una cadena */
